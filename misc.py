3c9cc49b18361aeb7a2309226517c872b6471133
Rishmika <111166308+Rishmika@users.noreply.github.com>
2022-12-23 12:56:51 +0530

    Update misc.py

--- misc.py
+++ misc.py
@@ -1,18 +1,18 @@
-HELP_TEXT = """__**I Can Play Music In The Voice Chat**__
+HELP_TEXT = """__**මට Voice Chat එකේ Music Play කරන්න පුළුවන්**__
 
-**/skip** __Skip The Current Playing Music.__
-**/play** __Service_Or_Default (Services: youtube/saavn, Default: youtube) Song_Name | Reply_To_Audio__
-**/joinvc** __Join Voice Chat.__
-**/leavevc** __Leave Voice Chat.__
-**/volume [1-200]** __Adjust Volume.__
-**/pause** __Pause Music.__
-**/resume** __Resume Music.__
-**/queue plformat/Nothing** __Shows Queue List. If you send plformat with command you will get it in playlist format.__
-**/delqueue** __Deletes Queue List and Playlist.__
-**/playlist** __Start Playing Playlist.__
+**/skip** __දැනට වාදනය වන සංගීතය මඟ හරින්න.__
+**/play** __Service_Or_Default (සේවා: youtube/saavn, Default: youtube) Song_Name | ශ්‍රව්‍ය වෙත_පිළිතුරු දෙන්න__
+**/joinvc** __Voice Chat එකට එකතු වෙන්න.__
+**/leavevc** __Voice Chat හැර යන්න.__
+**/volume [1-200]** __පරිමාව සකසන්න.__
+**/pause** __සංගීතය විරාම කරන්න.__
+**/resume** __සංගීතය නැවත ආරම්භ කරන්න.__
+**/queue plformat/Nothing** __පෝලිම් ලැයිස්තුව පෙන්වයි. ඔබ විධානය සමඟ plformat යවන්නේ නම් ඔබට එය ධාවන ලැයිස්තු ආකෘතියෙන් ලැබෙනු ඇත.__
+**/delqueue** __පෝලිම් ලැයිස්තුව සහ ධාවන ලැයිස්තුව මකා දමයි.__
+**/playlist** __ධාවන ලැයිස්තුව ක්‍රීඩා කිරීම ආරම්භ කරන්න.__
 """
 
 REPO_TEXT = (
-    "[Github](https://github.com/thehamkercat/Telegram_vc_bot)"
-    + " | [Group](t.me/TGVCSUPPORT)"
+    "අනේ මගේ පුක රිදෙනවා "
+    + " | [Group](t.me/EmoBotSupport)"
 )
